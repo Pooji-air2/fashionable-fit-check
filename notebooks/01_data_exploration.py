135f6b8ccfa425334652983e25037c208819cb2f
Savalapu Poojitha <poojithasavalapu@gmail.com>
2026-09-06 13:21:38 +0530

    Start Day 2 data exploration

--- notebooks/01_data_exploration.py
+++ notebooks/01_data_exploration.py
@@ -1,1 +1,8 @@
+# Fashionable Fit Check
+# Day 2 - Data Exploration
 
+import pandas as pd
+import numpy as np
+
+print("Fashionable Fit Check - Data Exploration")
+print("Day 2 started successfully!")
